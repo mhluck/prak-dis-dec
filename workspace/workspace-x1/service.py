8844786edec9609d124a3cac7fc1b030bc862a91
mhluck <moh.luqman.noer@gmail.com>
2026-05-12 18:20:38 +0700

    Microservices

---
+++ workspace/workspace-x1/service.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+from fastapi import Depends, FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class Sdm(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    npp: str
+    nama: str
+
+engine = create_engine("sqlite:///departemen-sdm.db")
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+app = FastAPI()
+
+@app.get("/sdm/", response_model=List[Sdm])
+def read_sdm(session: Session = Depends(get_session)):
+    # SQLModel select statement
+    statement = select(Sdm)
+    results = session.exec(statement).all()
+    return results
+
